f81cc8c69ef4754995caa14a198756e1fc3362a3
Mohammed Abad <test52308@gmail.com>
2024-01-17 17:51:27 +0100

    Ajout des class tools

---
+++ src/main/java/com/ehei/tools/ConnectionDB.java
@@ -0,0 +1,34 @@
+package com.ehei.tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.apache.logging.log4j.Level;
+
+public class ConnectionDB {
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            final String DB_NAME = "blogeDatabase";
+            final String DB_URL = "jdbc:mysql://127.0.0.1:3306/" + DB_NAME + "?useSSL=false";
+            final String DB_USERNAME = "root";
+            final String DB_PASSWORD = "password";
+
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            Tools.log(Level.INFO, "Connecté au base de données.");
+        } catch (Exception e) {
+            Tools.log(Level.ERROR, "Impossible de se connecter a la base de données.");
+            Tools.log(Level.ERROR, e.getMessage());
+        }
+    }
+
+    private ConnectionDB() throws InstantiationException {
+        throw new InstantiationException("YOU CANNOT INSTANTIATE THIS CLASS, use getInstance()");
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
---
+++ src/main/java/com/ehei/tools/Tools.java
@@ -0,0 +1,52 @@
+package com.ehei.tools;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+
+import static java.time.temporal.ChronoUnit.HOURS;
+
+public final class Tools {
+    private static final Logger logger = LogManager.getLogger(Tools.class);
+
+    public Tools() throws InstantiationException {
+        throw new InstantiationException("YOU CANNOT INSTANTIATE THIS CLASS, use getInstance()");
+    }
+
+
+    public static void log(Level level, String message) {
+        logger.log(level, message);
+    }
+
+
+    public static String toMd5(String value) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] inputBytes = value.getBytes();
+            byte[] hashBytes = md.digest(inputBytes);
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xFF & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            log(Level.ERROR, e.getMessage());
+            return null;
+        }
+    }
+
+    public static long getHourDifference(LocalDateTime startDate, LocalDateTime endDate) {
+        return HOURS.between(endDate, startDate);
+    }
+
+
+}
+
