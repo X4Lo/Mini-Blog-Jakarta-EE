3cd30d3cf1e69b73c93f63e53c7de4658ee8ef8d
Mohammed Abad <test52308@gmail.com>
2024-01-20 21:35:04 +0100

    Supression des fonction non utilisé du classe tools

--- src/main/java/com/ehei/tools/Tools.java
+++ src/main/java/com/ehei/tools/Tools.java
@@ -1,22 +1,11 @@
 package com.ehei.tools;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.Part;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import static java.time.temporal.ChronoUnit.HOURS;
 
 public final class Tools {
     private static final Logger logger = LogManager.getLogger(Tools.class);
@@ -50,30 +39,6 @@ public final class Tools {
             return null;
         }
     }
-
-    public static long getHourDifference(LocalDateTime startDate, LocalDateTime endDate) {
-        return HOURS.between(endDate, startDate);
-    }
-
-    public static String saveFile(HttpServlet servlet, HttpServletRequest request, String file) throws ServletException, IOException {
-        Part filePart = request.getPart(file); // Récupère le fichier envoyé
-        String originalFileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
-
-        String newFileName = UUID.randomUUID() + "_" + originalFileName;
-
-        String uploadPath = servlet.getServletContext().getRealPath("") + File.separator + "uploads";
-        File uploadsDir = new File(uploadPath);
-        if (!uploadsDir.exists()) {
-            uploadsDir.mkdir();
-        }
-
-        File fileToSave = new File(uploadsDir, newFileName);
-        System.out.println("Enregistrement du fichier à : " + uploadPath + File.separator + newFileName);
-        filePart.write(fileToSave.getAbsolutePath());
-
-        return newFileName;
-    }
-
 
 }
 
